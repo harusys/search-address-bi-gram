c065f24deaeaefc71f29aeea104d86cea7e13140
harusys <56357240+harusys@users.noreply.github.com>
2022-07-24 04:39:06 +0000

    [upd] インデックス重複排除、インデックスを郵便番号 -> 行番号に変更

--- src/search_index.rb
+++ src/search_index.rb
@@ -7,39 +7,45 @@ DATA_PATH = File.expand_path('../data/KEN_ALL_PART.CSV', __dir__)
 
 # N-Gram 作成
 class String
-  def to_ngram(n)
+  def to_ngram(n_str)
     each_char
-      .each_cons(n)
+      .each_cons(n_str)
       .map(&:join)
+      .uniq # 重複を除去（例：京都府京都市 -> "京都"インデックスは１つ）
   end
 end
 
-def search_index(word)
-  # インデックス作成
-  index = Hash.new { |h, k| h[k] = [] }
-  CSV.foreach(DATA_PATH, encoding: 'SJIS:UTF-8').each_slice(BATCH_SIZE) do |rows|
-    rows.each do |row|
-      postal_code = row[2]
-      address = "#{row[6]}#{row[7]}#{row[8]}"
-      # 住所をバイグラムで分割
-      address.to_ngram(2).each do |ngram|
-        index[ngram] << postal_code
-      end
-    end
-  end
+# クエリ
+# TODO: 三文字以上のクエリを指定できるようにする
+query = ARGV[0]
 
-  # クエリ
-  # TODO: 三文字以上のクエリを指定できるようにする
-  query = word
-
-  # 検索
-  # メモリリークを避けるため、全量をメモリにのせず、地道に検索する
-  CSV.foreach(DATA_PATH, encoding: 'SJIS:UTF-8').each_slice(BATCH_SIZE) do |rows|
-    rows.each do |row|
-      p row if index[query].include?(row[2])
-    end
-  end
+# パラメータがない or ２つ以上の場合、使用法を表示
+if ARGV.size != 1
+  puts 'コマンドライン引数に検索したい文字列を１つ指定してください。'
+  puts '例）ruby search_index.rb 京都'
+  return
 end
 
-# 関数実行
-search_index('京都')
+# インデックス作成
+# TODO: 「以下に掲載がない場合」「・・の次に番地がくる場合」削除処理
+# TODO: 郵便番号の重複処理（例：4980000）　※全国地方公共団体コードと複合キーで検索する？
+# TODO: 複数行に分割されるレコード処理（例：0330072）　※（ が始まり ）で終わるまでが分割されたレコードと判定する
+index = Hash.new { |h, k| h[k] = [] }
+CSV.foreach(DATA_PATH, encoding: 'SJIS:UTF-8').each_slice(BATCH_SIZE) do |rows|
+  rows.each.with_index(1) do |row, idx|
+    address = "#{row[6]}#{row[7]}#{row[8]}"
+    # 住所をバイグラムで分割
+    address.to_ngram(2).each do |ngram|
+      index[ngram].push(idx)
+    end
+  end
+  puts index
+end
+
+# 検索
+# メモリリークを避けるため、全量をメモリにのせず、地道に検索する
+CSV.foreach(DATA_PATH, encoding: 'SJIS:UTF-8').each_slice(BATCH_SIZE) do |rows|
+  rows.each.with_index(1) do |row, idx|
+    p row if index[query].include?(idx)
+  end
+end
