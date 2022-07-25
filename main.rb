58f9d569ae8678829e42674090db5842af89eb30
harusys <56357240+harusys@users.noreply.github.com>
2022-07-25 05:27:13 +0000

    [upd] 検索文字列のスペース削除対応

--- main.rb
+++ main.rb
@@ -8,9 +8,9 @@ case ARGV.length
 when 0
   address_idx.rebuild
 when 1
-  address_idx.search(ARGV[0])
+  address_idx.search(ARGV[0].gsub(/(\s|　)+/, '')) # 検索文字列の全角/半角スペースは一律削除
 else
   puts 'コマンドライン引数は "空" または "検索したい文字列を１つ" を指定してください。'
   puts '例１） Create Index: ruby search_index.rb'
-  puts '例２） Search:       ruby search_index.rb 京都'
+  puts '例２） Search:       ruby search_index.rb "京都"'
 end
